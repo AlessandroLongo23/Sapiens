6b65dd433c9425175378984ddd8ea32312bd27bd
Alessandro Longo <longoa02@gmail.com>
2025-07-13 19:08:54 +0200

    default theme set to light

--- src/lib/stores/theme.js
+++ src/lib/stores/theme.js
@@ -1,7 +1,7 @@
 import { writable } from 'svelte/store';
 
 function createThemeStore() {
-    const { subscribe, set } = writable('dark');
+    const { subscribe, set } = writable('light');
 
     return {
         subscribe,
