39139736ecd33fb058379f4336a4feb42a5a0ac4
Tiziano <t.talini@libero.it>
2025-11-22 20:15:24 +0100

    authmodal per registrazione

--- src/lib/components/ui/modals/AuthModal.svelte
+++ src/lib/components/ui/modals/AuthModal.svelte
@@ -9,11 +9,14 @@
 
 	let { 
 		isOpen = $bindable(false),
-		onClose = () => {}
+		onClose = () => {},
+		register = false
 	} = $props();
 
 	let email = $state('');
 	let password = $state('');
+	let name = $state('')
+	let surname = $state('')
 	let error = $state(null);
 	let loading = $state(false);
 	let isNavigating = $state(false);
@@ -47,12 +50,49 @@
 			isNavigating = false;
 		}
 	}
+
+	const handleRegister = async (event) => {
+		if (isNavigating) return;
+
+		event.preventDefault();
+		loading = true;
+		error = null;
+
+		try {
+			const { data: { user }, error: error } = await supabase.auth.signUp({ 
+				email,
+				name,
+				surname,
+				password 
+			});
+
+			if (error) throw error;
+
+			isNavigating = true;
+			// const redirectPath = user?.user_metadata?.role === 'admin' ? '/admin/analytics' : '/student/materiale';
+			const redirectPath = '/student/dashboard';
+			await goto(redirectPath);
+		} catch (err) {
+			error = err.message;
+			if (error.includes('Invalid Registration')) {
+				error = 'Email o password errate. Riprova';
+			}
+		} finally {
+			loading = false;
+			isNavigating = false;
+		}
+	}
+
+	const switchMode = () => {
+		register = !register
+	}
+
 </script>
 
 <Modal 
 	bind:isOpen={isOpen} 
-	title="Accedi al tuo account" 
-	size="sm" 
+	title={register ? 'Registra il tuo account' : "Accedi al tuo account"} 
+	size={register ? "md" : "sm"} 
 	backgroundBlur="xs" 
 	onClose={onClose} 
 	classes="bg-white dark:bg-zinc-800 shadow-2xl rounded-2xl sm:rounded-3xl max-w-xs sm:max-w-sm"
@@ -61,7 +101,7 @@
 		<h3
 			class="text-lg sm:text-xl font-bold bg-gradient-to-r from-zinc-800 to-zinc-900 dark:from-zinc-200 dark:to-zinc-100 bg-clip-text text-transparent"
 		>
-			Accedi al tuo account
+			{register ? "Registra il tuo account" : "Accedi al tuo account"}
 		</h3>
 		<button
 			onclick={onClose}
@@ -81,6 +121,10 @@
 			class="flex-1 flex flex-col w-full justify-center gap-4 text-foreground"
 		>
 			<div class="flex flex-col gap-2">
+				{#if register}
+				<FormInput type="name" placeholder="Name" bind:value={name} onchange={() => error = null} />
+				<FormInput type="surname" placeholder="Surname" bind:value={surname} onchange={() => error = null} />
+				{/if}
 				<FormInput type="email" placeholder="Email" bind:value={email} onchange={() => error = null} />
 				<FormInput type="password" placeholder="Password" bind:value={password} onchange={() => error = null} />
 			</div>
@@ -88,6 +132,16 @@
 			<FormButton type="submit" disabled={loading}>
 				{loading ? 'Loading...' : 'Sign In'}
 			</FormButton>
+			<div class="d-flex justify-content-between">
+				{#if register}
+				<span class="d-block">Non sei ancora registrato?</span>
+				{:else}
+				<span class="d-block">Sei già registrato? Vai al</span>
+				{/if}
+				<a disabled={loading} onclick={switchMode}>
+					{loading ? 'Loading...' : register ? 'Registrati' : 'Login'}
+				</a>
+			</div>
 		</form>
 
 		{#if error}
