839ea9164344c5673b14e4608c03dcb61c42ad14
Tiziano <t.talini@libero.it>
2025-11-27 13:20:34 +0100

    added admin wiki strucutre, installed redis

---
+++ src/routes/admin/wiki/[level_id]/[subject_id]/[chapter_id]/[topic_id]/flashcards/+page.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+    import ContentComingSoon from '$lib/components/content/ContentComingSoon.svelte';
+
+    let { data } = $props();
+    let { navigation } = $derived(data);
+</script>
+
+<ContentComingSoon type="flashcards" navigation={navigation} />
